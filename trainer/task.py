aebd35c1bc74352b0b5f8978ab52d761220275c8
SoniaSoniSo <soniavazbal@gmail.com>
2021-01-22 19:18:42 +0000

    Functions download and preprocess data

--- trainer/task.py
+++ trainer/task.py
@@ -12,75 +12,50 @@ template. Please feel free to remove this, or the other
 (sklearn_main.py), or adapt as you need.
 """
 
+import argparse
+import logging.config
 
-def add(x, y):
-    """Add the given parameters.
+from tensorflow.keras import datasets
+from tensorflow.keras import models,layers,activations
+from tensorflow.keras import optimizers
+from tensorflow.keras import losses
+from tensorflow.keras import metrics
 
-    :param x: x value
-    :type x: int
-    :param y: y value
-    :type y: int
-    :return: the sum of x and y
-    :rtype: int
-    """
-    return x + y
+def _download_data():
+    train,test =datasets.mnist.load_data()
+    x_train,y_train = train
+    x_test,y_test=test
+    return x_train,y_train,x_test,y_test
 
+def train_and_evaluate(batch_size,epochs,job_dir,output_path):
 
-def subtract(x, y):
-    """Substract the given parameters.
+    #Download the data
+    x_train,y_train,x_test,y_test = _download_data()
+    #Preprocess the data
 
-    :param x: x value
-    :type x: int
-    :param y: y value
-    :type y: int
-    :return: the diff of x and y
-    :rtype: int
-    """
-    return x - y
+    #Build the model
 
+    #Train the model
 
-def multiply(x, y):
-    """Multiply the given parameters.
-
-    :param x: x value
-    :type x: int
-    :param y: y value
-    :type y: int
-    :return: the multiplication of x and y
-    :rtype: int
-    """
-    return x * y
-
-
-def divide(x, y):
-    """Divide the given parameters.
-
-    :param x: x value
-    :type x: int
-    :param y: y value
-    :type y: int
-    :return: the division of x by y
-    :rtype: int
-    """
-    return x / y
-
+    #Evaluate the model
+    pass
 
 def main():
     """Entry point for your module."""
-    logger = logging.getLogger()
-    x1 = 2
-    y1 = 3
-    logger.info('Realizando suma')
-    logger.debug('{x} + {y} = '.format(x=x1, y=y1) + str(add(x1, y1)))
-    logger.info('Realizando resta')
-    logger.debug('{x} - {y} = '.format(x=x1, y=y1) + str(subtract(x1, y1)))
-    logger.info('Realizando multiplicación')
-    logger.debug('{x} * {y} = '.format(x=x1, y=y1) + str(multiply(x1, y1)))
-    logger.info('Realizando división')
-    logger.debug('{x} / {y} = '.format(x=x1, y=y1) + str(divide(x1, y1)))
-    logger.debug('TERMINADO')
-    logger.critical('FAIL')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch-size',type = int, help ='Batch size for the training')
+    parser.add_argument('--epochs',type = int, help ='Number the epochs for the training')
+    parser.add_argument('--job-dir',default=None, required=False, help='Option for AI Platform')
+    parser.add_argument('--model-output-path', help ='Path to write the SaveModel format')
 
+    args= parser.parse_args()
+    batch_size = args.batch_size
+    epochs = args.epochs
+    job_dir = args.job_dir
+    output_path = args.model_output_path
+
+    train_and_evaluate(batch_size,epochs,job_dir,output_path)
+   
 
 if __name__ == "__main__":
     main()
